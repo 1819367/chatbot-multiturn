a1fb08afd5e472da20f02d037f097d0829acbde6
Elizabeth Porter <142852984+1819367@users.noreply.github.com>
2024-07-29 16:15:02 -0700

    Manage token usage with tiktoken

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 from openai import OpenAI
+import tiktoken
 
 client = OpenAI()
 
@@ -12,6 +13,12 @@ def get_api_chat_response_message(model, messages):
         messages = messages
     )
 
+    # Print the entire API response to evaluate token usage
+    # print("API Response:", api_response)
+
+    # Print token usage
+    # print("Token Usage:", api_response.usage)
+
     # extract the response text
     response_content = api_response.choices[0].message.content
 
@@ -19,6 +26,15 @@ def get_api_chat_response_message(model, messages):
     return response_content
 
 model = "gpt-3.5-turbo"
+
+# tiktoken function to encode prompts
+encoding = tiktoken.encoding_for_model(model)
+
+# print statement to print out the encoding variable
+# print(encoding)
+
+# variable set to the model's input token limit (16385-4096 = max 12289)
+token_input_limit = 12289
 
 # create a multi-turn conversation
 # an array that will store the chat history
@@ -32,19 +48,29 @@ while True:
     # at the start, write an if statement when the user_input string is empty
     if (user_input == ""):
         user_input = input("Chatbot: Hello there, I'm your helpful chatbot!  Type exit to end our chat. What is your name? ")
-        chat_history.append({
-            "role": "user",
-            "content": user_input
-        })
-
-    # write an else, once the user responds
+         # write an else, once the user responds
     else: 
         user_input = input("You:  ") #Prompt the user for input
-    
     # write an if statement for user to stop the loop using 'break'exit
     if user_input.lower() == "exit": #if ueser types "exit", stop the loop
-        break
-    
+        # break, replaced with exit()
+        exit()
+
+    # encode the user's input with tiktoken 
+    # user_input_encoded = encoding.encode(user_input)
+    # print the encoded under input
+    # print(user_input_encoded)
+
+    # update the token_count to be the length of the user input
+    token_count = len(encoding.encode(user_input))
+    # print the token count
+    # print(f"Token Count: {token_count}")
+
+    # code to check the user's prompt against the input token limit
+    if (token_count > token_input_limit):
+        print("Your prompt is too long.  Please try again.")
+        continue
+
     # add the user's input in the chat history
     chat_history.append({
         "role": "user",
